3926243eda2a61e91e6158c88954b172199e917d
JamieOTR <theriseotr@gmail.com>
2025-06-25 18:23:06 -0400

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; 'https://xzfiaosmflflmuckbfnz.supabase.co';
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Zmlhb3NtZmxmbG11Y2tiZm56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3NzI4NzQsImV4cCI6MjA1MDM0ODg3NH0.Hs8-Ej7-Ej7-Ej7-Ej7-Ej7-Ej7-Ej7-Ej7-Ej7-Ej7-Ej7';
 
